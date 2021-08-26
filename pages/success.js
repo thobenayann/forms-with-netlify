8ea7c29da4d57c424dde2380e0bad87b3a07a56c
thobenayann <63640040+thobenayann@users.noreply.github.com>
2021-08-26 16:09:05 +0200

    add success message

--- pages/success.js
+++ pages/success.js
@@ -1,3 +1,30 @@
+import Image from "next/image";
+import Link from "next/link"
+
+// Images
+import check from "../public/check.svg"
+import arrowLeft from "../public/arrow-left.svg"
+
 export default function Success() {
-    return <div>Form successfully submitted!</div>
+    return (
+    <section  className="flex flex-col items-center justify-center bg-[#FAFAFA] h-screen">
+        <header  className="flex flex-col items-center">
+            <h2 className="px-20 pt-20 pb-10">Votre message a bien été transmis !</h2>
+            <p>Nous vous répondrons dans les plus brefs délais.</p>
+        </header>
+        <main className="object-center w-20 h-20">
+            <Image src={check} alt="check icon" layout="responsive"/>
+        </main>
+        <footer className="flex flex-col justify-center items-center p-20">
+            <Link href="/">
+            <a className="w-10 h-10 mb-10 animate-bounce">
+                <Image src={arrowLeft} alt="arrow left" layout="responsive"/>
+            </a>
+            </Link>
+            <Link href="/">
+                <a className="text-blue-600 underline">Retourner à la page de contact</a>
+            </Link>
+        </footer>
+    </section>
+    )
   }
